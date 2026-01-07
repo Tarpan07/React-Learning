44c54154cf7663e6727057ed748fca6b3806b819
Tarpan <tarpansaikia@gmail.com>
2026-01-07 18:13:55 +0530

    React

---
+++ create-react-comp/create-first-comp/src/App.jsx
@@ -0,0 +1,13 @@
+import KgButton from "./KgButton";
+import Hello from "./Hello";
+function App(){
+  return <div>
+  <h1>
+    Hello World
+  </h1>
+  <Hello></Hello>
+  
+  </div>
+}
+
+export default App;
---
+++ create-react-comp/create-first-comp/src/Hello.jsx
@@ -0,0 +1,12 @@
+function Hello(){
+
+    let myName= 'Tarpan';
+    let fullName=()=>{
+        return 'Tarpan Saikia';
+    }
+    return <h3>
+        This is the future speaking. I am your master {fullName()}
+    </h3>
+}
+
+export default Hello;
