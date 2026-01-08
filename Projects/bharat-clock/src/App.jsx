b4faedc740bf76e84d2d75a3e8f7f18a7d480001
Tarpan <tarpansaikia@gmail.com>
2026-01-08 15:47:05 +0530

    Bharat-Clock

---
+++ Projects/bharat-clock/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ClockHeading from './components/ClockHeading'
+import CurrentTime from './components/CurrentTime'
+import ClockSlogan from './components/ClockSlogan'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return <center>
+    <ClockHeading />
+    <ClockSlogan />
+    <CurrentTime />
+  </center>
+}
+
+export default App
